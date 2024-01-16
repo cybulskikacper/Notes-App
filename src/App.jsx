96ed8943b33222976e9ca0c64d2215a86f108f4d
Kacper <kacpercybulski@icloud.com>
2024-01-16 21:47:47 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,23 @@ function App() {
 	//         : oldNote
 	// }))
 
+	/**
+	 * Challenge: complete and implement the deleteNote function
+	 *
+	 * Hints:
+	 * 1. What array method can be used to return a new
+	 *    array that has filtered out an item based
+	 *    on a condition?
+	 * 2. Notice the parameters being based to the function
+	 *    and think about how both of those parameters
+	 *    can be passed in during the onClick event handler
+	 */
+
+	function deleteNote(event, noteId) {
+		event.stopPropagation()
+		console.log('deleted note', noteId)
+	}
+
 	function findCurrentNote() {
 		return (
 			notes.find(note => {
@@ -67,7 +84,9 @@ function App() {
 						currentNote={findCurrentNote()}
 						setCurrentNoteId={setCurrentNoteId}
 						newNote={createNewNote}
-					/>
+						deleteNote={deleteNote}
+
+ 					/>
 					{currentNoteId && notes.length > 0 && <Editor currentNote={findCurrentNote()} updateNote={updateNote} />}
 				</Split>
 			) : (
